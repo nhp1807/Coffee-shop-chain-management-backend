53bc69706d438eb89858f5f1a9fb83eb5600d3f0
Phong Nguyen <nhp1807@gmail.com>
2024-10-14 20:22:17 +0700

    update: ci.yml and test api /ping

---
+++ src/main/java/com/example/coffee_shop_chain_management/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.coffee_shop_chain_management.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/ping")
+    public String ping(){
+        return "pong";
+    }
+}
