755624c6539e35291fb627db168308eade78cb34
Phong Nguyen <nhp1807@gmail.com>
2024-10-24 16:38:01 +0700

    Add: init admin account when start the application

--- src/main/java/com/example/coffee_shop_chain_management/entity/DetailImportOrderId.java
+++ src/main/java/com/example/coffee_shop_chain_management/entity/DetailImportOrderId.java
@@ -5,6 +5,7 @@ import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -18,5 +19,18 @@ public class DetailImportOrderId implements Serializable {
     Long materialId;
 
     // Constructors, Getters and setters, equals() and hashCode()
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DetailImportOrderId that = (DetailImportOrderId) o;
+        return Objects.equals(importOrderId, that.importOrderId) &&
+                Objects.equals(materialId, that.materialId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(importOrderId, materialId);
+    }
 }
 
--- src/main/java/com/example/coffee_shop_chain_management/entity/ProductMaterialId.java
+++ src/main/java/com/example/coffee_shop_chain_management/entity/ProductMaterialId.java
@@ -5,6 +5,7 @@ import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -18,5 +19,18 @@ public class ProductMaterialId implements Serializable {
     Long materialId;
 
     // Constructors, Getters and setters, equals() and hashCode()
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductMaterialId that = (ProductMaterialId) o;
+        return Objects.equals(productId, that.productId) &&
+                Objects.equals(materialId, that.materialId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, materialId);
+    }
 }
 
