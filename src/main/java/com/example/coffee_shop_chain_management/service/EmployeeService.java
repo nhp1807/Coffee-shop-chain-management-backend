dac9cda657ccd4581bf89757e7fa8c7499b98d64
Quang Hoang <hoang.nqh207605@sis.hust.edu.vn>
2024-11-04 22:17:46 +0700

    add st

--- src/main/java/com/example/coffee_shop_chain_management/service/EmployeeService.java
+++ src/main/java/com/example/coffee_shop_chain_management/service/EmployeeService.java
@@ -1,6 +1,9 @@
 package com.example.coffee_shop_chain_management.service;
 
+import com.example.coffee_shop_chain_management.dto.CreateEmployeeDTO;
+import com.example.coffee_shop_chain_management.entity.Branch;
 import com.example.coffee_shop_chain_management.entity.Employee;
+import com.example.coffee_shop_chain_management.repository.BranchRepository;
 import com.example.coffee_shop_chain_management.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,14 +14,33 @@ import java.util.List;
 public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
+    @Autowired
+    private BranchRepository branchRepository;
 
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
 
-    public Employee createEmployee(Employee employee) {
+    public Employee createEmployee(CreateEmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        employee.setName(employeeDTO.getName());
+        employee.setDob(employeeDTO.getDob());
+        employee.setPhone(employeeDTO.getPhone());
+        employee.setEmail(employeeDTO.getEmail());
+        employee.setAddress(employeeDTO.getAddress());
+        employee.setChatID(employeeDTO.getChatID());
+
+        Branch branch = branchRepository.findById(employeeDTO.getBranchID()).orElseThrow(() -> new RuntimeException("Branch not found"));
+        employee.setBranch(branch);
+
         return employeeRepository.save(employee);
     }
+
+//    public List<Employee> getAllEmployees()
+//    {
+//        List<Employee> all = EmployeeRepository.findAll();
+//        return all;
+//    }
 
     public Employee getEmployeeById(Long id) {
         return employeeRepository.findById(id).orElse(null);
